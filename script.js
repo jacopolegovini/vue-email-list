cef27f29da0b5c02e3275ed32e291ddddfa80c92
jacopolegovini <legovini96@gmail.com>
2024-09-11 16:53:31 +0200

    exercise completed - code polished

--- script.js
+++ script.js
@@ -2,29 +2,29 @@ const {createApp} = Vue;
 createApp({
     data() {
       return {
-        // email: '',
         emailsArray: [],
         apiUrl: 'https://flynn.boolean.careers/exercises/api/random/mail' 
     }
     },
+
     methods: {
         generateEmail() {
-            
-            for(let i = 0; i <= 10; i++) {
+            for(let i = 0; i < 10; i++) {
                 axios.get(this.apiUrl)
                     .then((response) => {
-                        
+                        console.log(response)
                         this.emailsArray.push(response.data.response)
-                        
-                        // return this.email
                     })
                 }
             console.log(this.emailsArray)
             return this.emailsArray;
         }           
     }, 
+
     computed: {
+
     }, 
+
     mounted() {
         this.generateEmail();
     }
